b5fdcd4f583a5923bac3a97e3d7264cd9a655a01
DasunNirmal <dasun200301@gmail.com>
2024-08-21 15:07:39 +0530

    init method and logs created

--- src/main/java/lk/ijse/ptobackend/CustomerController.java
+++ src/main/java/lk/ijse/ptobackend/CustomerController.java
@@ -2,17 +2,36 @@ package lk.ijse.ptobackend;
 
 import jakarta.servlet.ServletConfig;
 import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+import javax.sql.DataSource;
 import java.io.IOException;
+import java.sql.Connection;
+import java.sql.SQLException;
 
+@WebServlet(urlPatterns = "/customerController")
 public class CustomerController extends HttpServlet {
+
+    static Logger logger = LoggerFactory.getLogger(CustomerController.class);
+    Connection connection;
 
     @Override
     public void init(ServletConfig config) throws ServletException {
-
+        logger.info("Initializing CustomerController with calling init method");
+        try {
+            var ctx = new InitialContext();
+            DataSource pool = (DataSource) ctx.lookup("java:comp/env/jdbc/ptoBackend");
+            this.connection = pool.getConnection();
+        } catch (NamingException | SQLException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     @Override
