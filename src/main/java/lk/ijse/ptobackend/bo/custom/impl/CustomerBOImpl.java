9911ec6b722634d05420b30f028e8f1d8f9cfd6f
DasunNirmal <dasun200301@gmail.com>
2024-08-23 15:46:10 +0530

    get all customers method

--- src/main/java/lk/ijse/ptobackend/bo/custom/impl/CustomerBOImpl.java
+++ src/main/java/lk/ijse/ptobackend/bo/custom/impl/CustomerBOImpl.java
@@ -8,6 +8,8 @@ import lk.ijse.ptobackend.entity.Customer;
 
 import java.sql.Connection;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 
 public class CustomerBOImpl implements CustomerBO {
 
@@ -16,5 +18,15 @@ public class CustomerBOImpl implements CustomerBO {
     @Override
     public boolean saveCustomer(CustomerDTO customerDTO, Connection connection) throws SQLException {
         return customerDAO.save(new Customer(customerDTO.getCustomerID(), customerDTO.getCustomerName(), customerDTO.getCustomerAddress(), customerDTO.getCustomerPhoneNumber()), connection);
+    }
+
+    @Override
+    public List<CustomerDTO> getAllCustomers(Connection connection) throws SQLException {
+        List<CustomerDTO> customerDTOS = new ArrayList<>();
+        List<Customer> customers = customerDAO.getAll(connection);
+        for (Customer customer : customers) {
+            customerDTOS.add(new CustomerDTO(customer.getCustomerID(), customer.getCustomerName(), customer.getCustomerAddress(), customer.getCustomerPhoneNumber()));
+        }
+        return customerDTOS;
     }
 }
