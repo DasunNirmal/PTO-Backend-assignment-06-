eccf5995ba10d97fe88a42e0d3dd5569a37d5edb
DasunNirmal <dasun200301@gmail.com>
2024-08-23 15:45:48 +0530

    get all customers method

--- src/main/java/lk/ijse/ptobackend/bo/custom/CustomerBO.java
+++ src/main/java/lk/ijse/ptobackend/bo/custom/CustomerBO.java
@@ -5,7 +5,10 @@ import lk.ijse.ptobackend.dto.CustomerDTO;
 
 import java.sql.Connection;
 import java.sql.SQLException;
+import java.util.List;
 
 public interface CustomerBO extends SuperBO {
     boolean saveCustomer(CustomerDTO customerDTO, Connection connection) throws SQLException;
+
+    List<CustomerDTO> getAllCustomers(Connection connection) throws SQLException;
 }
