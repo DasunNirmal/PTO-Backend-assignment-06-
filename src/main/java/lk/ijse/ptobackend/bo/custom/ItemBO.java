a5a98c07161fad74b9bf82d76c99ba1d13d405f6
DasunNirmal <dasun200301@gmail.com>
2024-08-25 15:21:37 +0530

    Save and Get All DOne

--- src/main/java/lk/ijse/ptobackend/bo/custom/ItemBO.java
+++ src/main/java/lk/ijse/ptobackend/bo/custom/ItemBO.java
@@ -1,4 +1,14 @@
 package lk.ijse.ptobackend.bo.custom;
 
-public interface ItemBO {
+import lk.ijse.ptobackend.bo.SuperBO;
+import lk.ijse.ptobackend.dto.ItemDTO;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.util.List;
+
+public interface ItemBO extends SuperBO {
+    boolean saveItem(ItemDTO itemDTO, Connection connection) throws SQLException;
+
+    List<ItemDTO> getAllCustomers(Connection connection) throws SQLException;
 }
