da0733b468463ae807b7fb695d833cf8e48c91b5
DasunNirmal <dasun200301@gmail.com>
2024-08-25 15:25:25 +0530

    Save and Get All Done

--- src/main/java/lk/ijse/ptobackend/controller/ItemController.java
+++ src/main/java/lk/ijse/ptobackend/controller/ItemController.java
@@ -1,4 +1,104 @@
 package lk.ijse.ptobackend.controller;
 
-public class ItemController {
+import jakarta.json.bind.Jsonb;
+import jakarta.json.bind.JsonbBuilder;
+import jakarta.json.bind.JsonbException;
+import jakarta.servlet.ServletConfig;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lk.ijse.ptobackend.bo.BOFactory;
+import lk.ijse.ptobackend.bo.custom.ItemBO;
+import lk.ijse.ptobackend.dto.CustomerDTO;
+import lk.ijse.ptobackend.dto.ItemDTO;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+import javax.sql.DataSource;
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.util.List;
+
+@WebServlet(urlPatterns = "/itemController")
+public class ItemController extends HttpServlet {
+
+    private Connection connection;
+    static Logger logger = LoggerFactory.getLogger(ItemController.class);
+    ItemBO itemBO = (ItemBO) BOFactory.getBoFactory().getBO(BOFactory.BOTypes.ITEMS);
+
+    @Override
+    public void init(ServletConfig config) throws ServletException {
+        logger.info("Initializing ItemController with calling init method");
+        try {
+            var ctx = new InitialContext();
+            DataSource pool = (DataSource) ctx.lookup("java:comp/env/jdbc/ptoBackend");
+            this.connection = pool.getConnection();
+        } catch (NamingException | SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        /*Todo: Save Details*/
+        if (!req.getContentType().toLowerCase().startsWith("application/json") || req.getContentType() == null) {
+            resp.sendError(HttpServletResponse.SC_BAD_REQUEST);
+        }
+
+        try(var writer = resp.getWriter()) {
+            Jsonb jsonb = JsonbBuilder.create();
+            ItemDTO itemDTO = jsonb.fromJson(req.getReader(), ItemDTO.class);
+
+            boolean iSaved = itemBO.saveItem(itemDTO, connection);
+            if (iSaved) {
+                writer.write("Item saved successfully");
+                resp.setStatus(HttpServletResponse.SC_CREATED);
+            } else {
+                writer.write(" Something went wrong Item did not saved successfully");
+                resp.sendError(HttpServletResponse.SC_BAD_REQUEST);
+            }
+        } catch (JsonbException | SQLException e) {
+            resp.setStatus(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
+            throw new RuntimeException(e);
+        }
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        /*Todo: Get Details*/
+        loadAllItems(req, resp);
+    }
+
+    private void loadAllItems(HttpServletRequest req, HttpServletResponse resp) {
+        try (var writer = resp.getWriter()) {
+            List<ItemDTO> iteDTOList = itemBO.getAllCustomers(connection);
+            if (iteDTOList != null) {
+                resp.setContentType("application/json");
+                Jsonb jsonb = JsonbBuilder.create();
+                jsonb.toJson(iteDTOList, writer);
+            } else {
+                writer.write("No items found");
+                resp.setStatus(HttpServletResponse.SC_NOT_FOUND);
+            }
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @Override
+    protected void doPatch(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        /*Todo: Update Details*/
+        super.doPatch(req, resp);
+    }
+
+    @Override
+    protected void doDelete(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        /*Todo: Delete Details*/
+        super.doDelete(req, resp);
+    }
 }
