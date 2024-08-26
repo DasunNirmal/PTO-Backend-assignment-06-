31d22c503d44444fb4185bccd71f38f44c3c140d
DasunNirmal <dasun200301@gmail.com>
2024-08-27 02:20:44 +0530

    Ordrs Can Be Saved

--- src/main/java/lk/ijse/ptobackend/bo/custom/OrderBO.java
+++ src/main/java/lk/ijse/ptobackend/bo/custom/OrderBO.java
@@ -1,4 +1,11 @@
 package lk.ijse.ptobackend.bo.custom;
 
-public interface OrderBO {
+import lk.ijse.ptobackend.bo.SuperBO;
+import lk.ijse.ptobackend.dto.CombinedOrderDTO;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+
+public interface OrderBO extends SuperBO {
+    boolean saveOrder(CombinedOrderDTO combinedOrderDTO, Connection connection) throws SQLException;
 }
