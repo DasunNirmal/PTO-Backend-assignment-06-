390891122df615030b420deb32a219d6a474a6e5
DasunNirmal <dasun200301@gmail.com>
2024-08-27 02:18:32 +0530

    CrudDAO extened

--- src/main/java/lk/ijse/ptobackend/dao/custom/ItemDAO.java
+++ src/main/java/lk/ijse/ptobackend/dao/custom/ItemDAO.java
@@ -3,5 +3,9 @@ package lk.ijse.ptobackend.dao.custom;
 import lk.ijse.ptobackend.dao.CrudDAO;
 import lk.ijse.ptobackend.entity.Item;
 
+import java.sql.Connection;
+import java.sql.SQLException;
+
 public interface ItemDAO extends CrudDAO<Item> {
+    boolean updateQty(Item item, Connection connection) throws SQLException;
 }
