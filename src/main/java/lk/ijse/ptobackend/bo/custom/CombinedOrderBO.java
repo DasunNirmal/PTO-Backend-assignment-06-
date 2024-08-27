244a727320c1ccfe43f853ca3a21a6f6c653e40a
DasunNirmal <dasun200301@gmail.com>
2024-08-27 12:57:20 +0530

    get all orders method

--- src/main/java/lk/ijse/ptobackend/bo/custom/CombinedOrderBO.java
+++ src/main/java/lk/ijse/ptobackend/bo/custom/CombinedOrderBO.java
@@ -1,4 +1,12 @@
 package lk.ijse.ptobackend.bo.custom;
 
-public interface CombinedOrderBO {
+import lk.ijse.ptobackend.bo.SuperBO;
+import lk.ijse.ptobackend.dto.CombinedOrderDTO;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.util.List;
+
+public interface CombinedOrderBO extends SuperBO {
+    List<CombinedOrderDTO> getAllOrders(Connection connection) throws SQLException;
 }
