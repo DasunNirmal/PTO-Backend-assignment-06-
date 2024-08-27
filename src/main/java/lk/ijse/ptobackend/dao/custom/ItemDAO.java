3cf48257c28a9caba6247a79870cff70c7f9fbaf
DasunNirmal <dasun200301@gmail.com>
2024-08-28 01:52:23 +0530

    Debuged

--- src/main/java/lk/ijse/ptobackend/dao/custom/ItemDAO.java
+++ src/main/java/lk/ijse/ptobackend/dao/custom/ItemDAO.java
@@ -9,6 +9,5 @@ import java.sql.SQLException;
 public interface ItemDAO extends CrudDAO<Item> {
     boolean updateQty(Item item, Connection connection) throws SQLException;
     boolean updateQtyDeleted(String itemID, String orderQty, Connection connection) throws SQLException;
-
     boolean updateQtyOnHand(String itemID, String qtyOnHand, Connection connection) throws SQLException;
 }
