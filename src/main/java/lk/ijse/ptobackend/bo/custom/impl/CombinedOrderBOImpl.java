d2427efc2849856116a7107e645d8ac647c20dc3
DasunNirmal <dasun200301@gmail.com>
2024-08-27 12:56:40 +0530

    get all orders method

--- src/main/java/lk/ijse/ptobackend/bo/custom/impl/CombinedOrderBOImpl.java
+++ src/main/java/lk/ijse/ptobackend/bo/custom/impl/CombinedOrderBOImpl.java
@@ -1,4 +1,28 @@
-package lk.ijse.ptobackend.bo.custom;
+package lk.ijse.ptobackend.bo.custom.impl;
 
-public class CombinedOrderBOImpl {
+import lk.ijse.ptobackend.bo.custom.CombinedOrderBO;
+import lk.ijse.ptobackend.dao.DAOFactory;
+import lk.ijse.ptobackend.dao.custom.CombinedOrderDAO;
+import lk.ijse.ptobackend.dto.CombinedOrderDTO;
+import lk.ijse.ptobackend.entity.CombinedOrder;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class CombinedOrderBOImpl implements CombinedOrderBO {
+
+    CombinedOrderDAO combinedOrderDAO = (CombinedOrderDAO) DAOFactory.getDaoFactory().getDAO(DAOFactory.DAOTypes.COMBINED_ORDER);
+
+    @Override
+    public List<CombinedOrderDTO> getAllOrders(Connection connection) throws SQLException {
+        List<CombinedOrderDTO> combinedOrderDTOS = new ArrayList<>();
+        List<CombinedOrder> combinedOrders = combinedOrderDAO.getAll(connection);
+        for (CombinedOrder cO : combinedOrders) {
+            combinedOrderDTOS.add(new CombinedOrderDTO(cO.getOrderID(),cO.getOrderDate(),cO.getCustomerID(),cO.getItemID(),
+                    cO.getItemName(),cO.getItemPrice(),cO.getItemQty(),cO.getOrderQty(),cO.getTotalPrice()));
+        }
+        return combinedOrderDTOS;
+    }
 }
